7c2708c6816ce34cf7266b69ff998bb6bacfda89
dadams2 <dbadams@iinet.net.au>
2020-05-14 16:02:33 +0800

    part f implementation

--- project/LINUX_VERSIONS/fStart.glsl
+++ project/LINUX_VERSIONS/fStart.glsl
@@ -1,9 +1,56 @@
-varying vec4 color;
+
 varying vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+varying vec4 position; 
+varying vec3 normal;
+
 
 uniform sampler2D texture;
 
+vec4 color;
+
+uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+uniform mat4 ModelView;
+uniform mat4 Projection;
+uniform vec4 LightPosition;
+uniform float Shininess;
+
 void main()
 {
+//IMPLEMENTATION of part F
+
+    vec3 pos = (ModelView * position).xyz;
+
+
+    // The vector to the light from the vertex    
+    vec3 Lvec = LightPosition.xyz - pos;
+
+    // Unit direction vectors for Blinn-Phong shading calculation
+    vec3 L = normalize( Lvec );   // Direction to the light source
+    vec3 E = normalize( -pos );   // Direction to the eye/camera
+    vec3 H = normalize( L + E );  // Halfway vector
+
+    // Transform vertex normal into eye coordinates (assumes scaling
+    // is uniform across dimensions)
+    vec3 N = normalize( (ModelView*vec4(normal, 0.0)).xyz );
+
+    // Compute terms in the illumination equation
+    vec3 ambient = AmbientProduct;
+
+    float Kd = max( dot(L, N), 0.0 );
+    vec3  diffuse = Kd*DiffuseProduct;
+
+    float Ks = pow( max(dot(N, H), 0.0), Shininess );
+    vec3  specular = Ks * SpecularProduct;
+    
+    if (dot(L, N) < 0.0 ) {
+	specular = vec3(0.0, 0.0, 0.0);
+    } 
+
+    // globalAmbient is independent of distance from the light source
+    vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
+//  PART F IMPLEMENTATION
+    color.rgb = (globalAmbient + ambient + diffuse + specular)/ (sqrt(dot(Lvec, Lvec))/14.0 + 1.0); //slightly better than inverse square
+    color.a = 1.0;
+
     gl_FragColor = color * texture2D( texture, texCoord * 2.0 );
 }
